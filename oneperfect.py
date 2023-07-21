ca1f9b4b18b4fe6d01d73e18c9b7870372db6165
yijia <13262866275@163.com>
2023-07-22 00:56:35 +0800

    更新模型导入方式

--- oneperfect.py
+++ oneperfect.py
@@ -6,6 +6,7 @@ from flask import Flask, request, jsonify, Response, make_response,stream_with_c
 from flask_cors import CORS
 import config
 import model
+from model import generate_online,generate_dalle,generate_baidu
 
 
 app = Flask(__name__)
@@ -36,19 +37,19 @@ def send_message():
     # 调用官方3.5 联网
     if model_name =="gpt-3.5-online" and oneperfect["gpt-online"]:    # 添加自定义模型的名称
 
-        response = Response(stream_with_context(model.generate_online(key,message)), content_type='text/event-stream')
+        response = Response(stream_with_context(generate_online(key,message)), content_type='text/event-stream')
         return response
 
     # 调用官方画图
     if model_name == "image" and oneperfect["gpt-image"]:
 
-        response = Response(stream_with_context(model.generate_dalle(key,message)), content_type='text/event-stream')
+        response = Response(stream_with_context(generate_dalle(key,message)), content_type='text/event-stream')
         return response
 
     # 调用百度文心一言
     if model_name =="wxyy" and oneperfect["baidu-wxyy"]:
 
-        response = Response(stream_with_context(model.generate_baidu(key,message)), content_type='text/event-stream')
+        response = Response(stream_with_context(generate_baidu(key,message)), content_type='text/event-stream')
         return response
 
     else:
